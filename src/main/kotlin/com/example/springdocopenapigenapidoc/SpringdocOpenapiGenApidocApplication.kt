2e90c1edec6984b2bb24f22c0cbb2c36ae9c038d
Msksgm <msksgm3124@gmail.com>
2023-12-26 08:57:03 +0900

    feat: プロジェクトを初期化

---
+++ src/main/kotlin/com/example/springdocopenapigenapidoc/SpringdocOpenapiGenApidocApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springdocopenapigenapidoc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringdocOpenapiGenApidocApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringdocOpenapiGenApidocApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springdocopenapigenapidoc/SpringdocOpenapiGenApidocApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springdocopenapigenapidoc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringdocOpenapiGenApidocApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
