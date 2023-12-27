6623fbc47f18271a0e3ea74ca8274fd57d134039
Msksgm <msksgm3124@gmail.com>
2023-12-27 20:31:47 +0900

    chore: OpenAPIDefinition アノテーションを記述

--- src/main/kotlin/com/example/springdocopenapigenapidoc/SpringdocOpenapiGenApidocApplication.kt
+++ src/main/kotlin/com/example/springdocopenapigenapidoc/SpringdocOpenapiGenApidocApplication.kt
@@ -1,5 +1,10 @@
 package com.example.springdocopenapigenapidoc
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.info.License
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,6 +12,29 @@ import org.springframework.boot.runApplication
  * SpringdocOpenapiGenApidocApplication
  */
 @SpringBootApplication
+@OpenAPIDefinition(
+    info = Info(
+        title = "API Title",
+        version = "0.0",
+        description = "My API",
+        license = License(name = "Apache 2.0", url = "http://sample"),
+        contact = Contact(url = "http://sample.com", name = "sample", email = "sample@sample.com")
+    ),
+    servers = [
+        Server(
+            description = "ローカルサーバーの説明です",
+            url = "http://localhost:8080",
+        ),
+        Server(
+            description = "事前環境サーバーの説明です",
+            url = "http://stg:8080",
+        ),
+        Server(
+            description = "本番環境サーバーの説明です",
+            url = "http://prod:8080",
+        ),
+    ],
+)
 class SpringdocOpenapiGenApidocApplication
 
 /**
